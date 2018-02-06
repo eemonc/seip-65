6e009660dcb19ae2d5015d8f2629f228411b3eca
Bappabd24 <bappachakraborty24@gmail.com>
2018-02-06 10:48:22 +0600

    firts nav

---
+++ js/custom.js
@@ -0,0 +1,102 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+// second navbar
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
+// second navbar
+
+// carosul
+  $('#myCarousel').carousel({
+  interval: 3500
+});
+
+// This event fires immediately when the slide instance method is invoked.
+$('#myCarousel').on('slide.bs.carousel', function (e) {
+    var id = $('.item.active').data('slide-number');
+    
+    // Added a statement to make sure the carousel loops correct
+    if(e.direction == 'right'){
+      id = parseInt(id) - 1;  
+      if(id == -1) id = 7;
+    } else{
+        id = parseInt(id) + 1;
+        if(id == $('[id^=carousel-thumb-]').length) id = 0;
+    }
+  
+    $('[id^=carousel-thumb-]').removeClass('selected');
+    $('[id=carousel-thumb-' + id + ']').addClass('selected');
+});
+
+// Thumb control
+$('[id^=carousel-thumb-]').click( function(){
+  var id_selector = $(this).attr("id");
+  var id = id_selector.substr(id_selector.length -1);
+  id = parseInt(id);
+  $('#myCarousel').carousel(id);
+  $('[id^=carousel-thumb-]').removeClass('selected');
+  $(this).addClass('selected');
+});
+// carosul
+
+// 2nd car
+  jQuery(document).ready(function($) {
+ 
+        $('#myCarousel').carousel({
+                interval: 5000
+        });
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
+// 2nd car
+
+
